a28d430d1b69a53ec1d96a87e2544599d66b62fb
D Tim Cummings <tim@triptera.com.au>
2020-04-24 23:14:45 +1000

    solution task 9

--- bpaml_dash_graph.py
+++ bpaml_dash_graph.py
@@ -26,36 +26,58 @@ app.layout = html.Div(children=[
         figure=figure_cumulative_doubling.fig_for_location(data=data),
     ),
 
-    html.Label("Country", htmlFor="input-country"),
-    dcc.Dropdown(
-        id="input-country",
-        value="",
-        options=[{"label": country, "value": country} for country in ar_country],
-    ),
+    html.Div(children=[
+        html.Div(children=[
+            html.Label("Country", htmlFor="input-country"),
+            dcc.Dropdown(
+                id="input-country",
+                value="",
+                options=[{"label": country, "value": country} for country in ar_country],
+            ),
+            html.Label("State", htmlFor="input-state"),
+            dcc.Dropdown(
+                id="input-state",
+                value="",
+                options=[],
+            ),
+        ], style={"breakInside": "avoid"}),
 
-    html.Label("State", htmlFor="input-state"),
-    dcc.Dropdown(
-        id="input-state",
-        value="",
-        options=[],
-    ),
+        html.Div(children=[
+            html.Label("Averaged days", htmlFor="input-averaged-days"),
+            dcc.Slider(
+                id="input-averaged-days",
+                min=1,
+                max=10,
+                value=5,
+                marks={str(n+1): str(n+1) for n in range(10)}
+            ),
+            html.Label("Starting number", htmlFor="input-num-start"),
+            dcc.Input(
+                id="input-num-start",
+                min=1,
+                value=100,
+                type="number",
+            ),
+        ], style={"breakInside": "avoid"}),
 
-    html.Label("Averaged days", htmlFor="input-averaged-days"),
-    dcc.Slider(
-        id="input-averaged-days",
-        min=1,
-        max=10,
-        value=5,
-        marks={str(n+1): str(n+1) for n in range(10)}
-    ),
+        html.Div(children=[
+            html.Label("Case type", htmlFor="input-case-type"),
+            dcc.RadioItems(
+                id="input-case-type",
+                value="confirmed",
+                options=[{"label": s, "value": s} for s in ("confirmed", "recovered", "deaths")],
+                labelStyle={"display": "inline-block"},
+            ),
+            html.Label("Graph type", htmlFor="input-yaxes-type"),
+            dcc.RadioItems(
+                id="input-yaxes-type",
+                value="log",
+                options=[{"label": s, "value": s} for s in ("linear", "log")],
+                labelStyle={"display": "inline-block"},
+            ),
+        ], style={"breakInside": "avoid"}),
+    ], style={"columnCount": 3}),
 
-    html.Label("Starting number", htmlFor="input-num-start"),
-    dcc.Input(
-        id="input-num-start",
-        min=1,
-        value=100,
-        type="number",
-    ),
 
     html.Div(children="", id="text-selected-country"),
 
@@ -92,13 +114,15 @@ def update_text_selected_country(country=None, state=None):
     [Input('input-country', 'value'),
      Input('input-state', 'value'),
      Input('input-num-start', 'value'),
-     Input('input-averaged-days', 'value')])
-def update_graph(country, state, num_start, averaged_days):
+     Input('input-case-type', 'value'),
+     Input('input-averaged-days', 'value'),
+     Input('input-yaxes-type', 'value')])
+def update_graph(country, state, num_start, case_type, averaged_days, yaxes_type):
     global data
     parameters = {}
     if num_start:
         parameters['num_start'] = num_start
-    return figure_cumulative_doubling.fig_for_location(data, country, state, averaged_days=averaged_days, **parameters)
+    return figure_cumulative_doubling.fig_for_location(data, country, state, case_type=case_type, averaged_days=averaged_days, yaxes_type=yaxes_type, **parameters)
 
 
 if __name__ == '__main__':
