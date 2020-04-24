b02d640b11024d0385135413a49ff54614200f85
D Tim Cummings <tim@triptera.com.au>
2020-04-24 14:13:41 +1000

    add solution task 4

--- bpaml_dash_graph.py
+++ bpaml_dash_graph.py
@@ -2,11 +2,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
 import data_cache
 
 data = data_cache.DataCache()
-lst_country = data.df_for_case_type()['Country/Region'].unique()
-lst_country.sort()
+ar_country = data.df_for_case_type()['Country/Region'].unique()
+ar_country.sort()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -23,10 +25,23 @@ app.layout = html.Div(children=[
     dcc.Dropdown(
         id="input-country",
         value="",
-        options=[{"label": country, "value": country} for country in lst_country],
+        options=[{"label": country, "value": country} for country in ar_country],
     ),
 
+    html.Div(children="", id="text-selected-country"),
+
 ])
+
+
+@app.callback(
+    Output('text-selected-country', 'children'),
+    [Input('input-country', 'value')])
+def update_text_selected_country(country):
+    if country:
+        return f"You have selected {country}"
+    else:
+        return f"You haven't selected any country"
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
