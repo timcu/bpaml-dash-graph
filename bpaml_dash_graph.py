92f4d17bd89524c1d57a79a5a1da7a222da285d1
D Tim Cummings <tim@triptera.com.au>
2020-04-24 21:33:26 +1000

    solution task 6

--- bpaml_dash_graph.py
+++ bpaml_dash_graph.py
@@ -5,6 +5,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 import data_cache
+import figure_cumulative_doubling
 import utilities
 
 data = data_cache.DataCache()
@@ -18,9 +19,12 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(children=[
     html.H1(children="Beginners' Python and Machine Learning - Dash web apps"),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    html.H2(children='COVID-19 data from Johns Hopkins CSSE'),
+
+    dcc.Graph(
+        id='graph-doubling-days',
+        figure=figure_cumulative_doubling.fig_for_location(data=data),
+    ),
 
     html.Label("Country", htmlFor="input-country"),
     dcc.Dropdown(
@@ -39,6 +43,11 @@ app.layout = html.Div(children=[
     html.Div(children="", id="text-selected-country"),
 
 ])
+
+# - figure_cumulative_doubling has been provided from meetup053
+# - add a dash_core_components Graph object with id="graph-doubling-days" and a figure=fig_for_location(data=data)
+# - define a function update_graph(country, state) which returns a figure based on selected country and state
+# - Use the @app.callback decorator to call the function when "value" of "input-country" or "input-state" changes
 
 
 @app.callback(
@@ -64,6 +73,15 @@ def update_ar_state(country):
      Input('input-state', 'value')])
 def update_text_selected_country(country=None, state=None):
     return f"You have selected {utilities.location_name(country, state)}"
+
+
+@app.callback(
+    Output('graph-doubling-days', 'figure'),
+    [Input('input-country', 'value'),
+     Input('input-state', 'value')])
+def update_graph(country=None, state=None):
+    global data
+    return figure_cumulative_doubling.fig_for_location(data, country, state)
 
 
 if __name__ == '__main__':
