d91087be54dbc01a034c678b3581ad9fe7239613
D Tim Cummings <tim@triptera.com.au>
2020-04-24 13:56:34 +1000

    task 3 solution

--- bpaml_dash_graph.py
+++ bpaml_dash_graph.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
+import data_cache
+
+data = data_cache.DataCache()
+lst_country = data.df_for_case_type()['Country/Region'].unique()
+lst_country.sort()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -12,6 +18,13 @@ app.layout = html.Div(children=[
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
+
+    html.Label("Country", htmlFor="input-country"),
+    dcc.Dropdown(
+        id="input-country",
+        value="",
+        options=[{"label": country, "value": country} for country in lst_country],
+    ),
 
 ])
 
